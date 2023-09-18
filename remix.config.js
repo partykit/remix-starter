c32fd6958e6abce18572d0aa075330a14ce41795
Sunil Pai <threepointone@gmail.com>
2023-09-18 11:18:28 +0100

    update to remix v2

--- remix.config.js
+++ remix.config.js
@@ -6,15 +6,4 @@ export default {
   serverMainFields: ["browser", "module", "main"],
   serverModuleFormat: "esm",
   serverPlatform: "neutral",
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
-  serverNodeBuiltinsPolyfill: {
-    modules: {},
-  },
 };
